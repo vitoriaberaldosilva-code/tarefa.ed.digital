faa02ea77015b69f3a324051af7562c0d88896be
vitoriaberaldosilva-code <vitoria.beraldo.silva@escola.pr.gov.br>
2026-09-16 13:45:29 -0300

    atualizei o script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-
+const botoesCurtir = document.querySelectorA11(".curtir");
+botoesCurtir.forEach(function(botaoCurtir){
+    let curtir = false;
+    botaoCurtir.addEventListener("click" , curtir);
+    function curtir(){
+        const contador = botaoCurtir.querySelector("span");
+        if(curtiu === false){
+            contador.textContent++;
+            curtiu = true;{
+                contador
+            }
+        }
+    }
+    
+})
